3d2adb1d2aae7e0e9670acf11785b356d195a2e6
Jhonah <buryakone@gmail.com>
2021-12-26 22:42:38 +0700

    result list

--- src/screens/home-screen/home-screen.view.js
+++ src/screens/home-screen/home-screen.view.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect, useCallback } from "react";
+import React, { useState, useEffect } from "react";
 import { getUsers } from "../../api/sibers.com.api";
 import Form from "./views/form/form.view";
-import ResultList from "./views/result-list/result-list.view";
-import FlatList from 'flatlist-react';
+import ResultsList from "./views/result-list/results-list.view";
 
 const App = () => {
     const [results, setResults] = useState([]);
@@ -13,36 +12,24 @@ const App = () => {
             const result = await getUsers();
 
             setResults(result);
-            // console.log(result)
         } catch (err) {
             alert("Ошибка в App.js");
         }
-    }
-
-    const renderItem = useCallback((user, id) => {
-        // console.log(user);
-        return (
-            <li key={id}>
-                <b>{user.name}</b>
-            </li>
-        );
-    }, []);
+    };
 
     useEffect(() => {
         getResults();
     }, []);
 
-    return (    
+    return ( 
+        <>
         <div>
             <h1>Welcome to React App</h1>
             <h3>Date : {new Date().toDateString()}</h3>            
-            <Form />            
-            <FlatList
-                list={results}
-                renderItem={renderItem}
-            />
-            {/* <ResultList results={results} /> */}
+            <Form />
+            <ResultsList results={results} />
         </div>
+        </>
     )
 }
 
---
+++ src/screens/home-screen/views/result-list/results-list.view.js
@@ -0,0 +1,45 @@
+import React, { useCallback } from "react";
+import "./results-list.view.css";
+import FlatList from 'flatlist-react';
+import ResultDetail from "../result-detail/result-detail.view";
+// import styled, { createGlobalStyle } from "styled-components";
+// // styled - стилизация компонента
+// // createGlobalStyle - глобальные стили
+// const StyledList = styled.ul`    
+//     display: flex;
+//     flex-direction: row;
+// `;
+
+const ResultList = ({ results }) => {
+    
+    // const keyExtractor = useCallback( item => item.id.toString(), []);
+    const renderPerson = useCallback( (person) => {
+        return (            
+            // <TouchableOpacity onPress={() => navigation.navigate('ResultsShow', { id: item.id })}>
+            // properties key -  avoid error in flatlist-react library
+            <ResultDetail result={person} key={person.id}/>
+            // </TouchableOpacity>
+        )}, []);
+
+    return (
+        <>
+            <h1> List Contacs </h1>
+            <h2> {results.length} </h2>
+            {/* <StyledList> */}
+            <ul>
+                <li>Имя</li>
+                <li>Телефон</li>
+                <li>Организация</li>
+                <li>Группа</li>
+            </ul>
+            {/* </StyledList> */}
+            <FlatList
+                list={results}
+                renderItem={renderPerson}
+                // keyExtractor={keyExtractor}
+            />            
+        </>
+    );
+};
+
+export default ResultList;
