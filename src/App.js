f94a92f704f37f01747e6d6fcbbab2069ab08e45
Jhonah <buryakone@gmail.com>
2021-12-29 00:46:19 +0700

    еще раз посмотреть нужно ли это свойство для передачи

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
                 <h1>Welcome to React Router!</h1>
                 <Routes>
                     <Route path="/" element={<HomeScreen />} />
-                    <Route path="about" element={<ResultShowScreen />} />
+                    <Route path="about" element={<ResultShowScreen authed={true}/>} />
                 </Routes>
             </div>
         </div>
