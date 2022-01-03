83be540a24e1dbba682a686f380fbc5337170ea9
Jhonah <buryakone@gmail.com>
2022-01-03 18:20:57 +0700

    refactoring the list output for a sequence of pages

--- src/screens/home-screen/views/result-list/results-list.view.js
+++ src/screens/home-screen/views/result-list/results-list.view.js
@@ -1,38 +1,76 @@
-import React, { useCallback } from "react";
+import React, { useReducer, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
 import FlatList from 'flatlist-react';
 import ResultDetail from "../result-detail/result-detail.view";
 import "./results-list.view.scss";
 
 const ResultList = ({ results }) => {
+    // const [limit, setLimit] = useState({
+    //     from: 0, 
+    //     to: 5, 
+    //     toString() {
+    //         return `from: ${this.from} to: ${this.to}`;
+    //     } 
+    // });
+    const initialState = {
+        from: 0, 
+        to: 5, 
+        toString() {
+            return `from: ${this.from} to: ${this.to}`;
+        } 
+    };
     
-    const renderPerson = useCallback( (person, idx) => {
+
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    function reducer (state, action) {
+        switch (action.type) {
+            case 'increment':
+                return {from: state.from + 5, to: state.to + 5};
+            case 'decrement':
+                return {from: state.from - 5, to: state.to - 5};
+            default:
+                throw new Error();
+        }
+    }
+
+    // (person, idx)
+    const renderPerson = useCallback( (person) => {
+    // const renderPerson = (person) => {
+        const ID = person.id;
+
+        if (!(ID >= state.from && ID < state.to)) return;
+
         return (
-            // <>
             <Link to='/about' 
-                  className="b123"
+                  className=""
                   // pass props throught state
                   state={{ from: person }}
                   // idx - unique key flatlist-react library
-                  key={idx}>
-            {/* // <a key={idx}> */}
-                {/* // <input onPress={() => navigation.navigate('ResultsShow', { id: person.id })}> */}
-                {/* // properties key -  avoid error in flatlist-react library */}
+                  // key={idx}>
+                  // properties key -  avoid error in flatlist-react library
+                  key={ID}>
+                  {/* <input onPress={() => navigation.navigate('ResultsShow', { id: person.id })}> */}
                 <ResultDetail result={person}/>
-                {/* <Link to="/about" >About</Link> */}
-                {/* </input>   */}
-                {/* </> */}
             </Link>
-        )}, []);
+        )}, [state]);
 
-    const mouseOverHandler = (e) => {
-        e.addEventListener("click", () => { alert("123") }, false);
-    };  
+    // const keyExtractor = (e) => {
+    // // useCallback( (e) => {
+    //     e.preventDefault();
+    //     const updateValue= {}
+    //     updateValue["from"] = 3
+    //     // const obj = {from: 5, to: 10};
+    //     setLimit(prevState => {
+    //         return {...prevState, ...updateValue};
+    //     });
+    //     alert(limit.toString());
+    // };
+    // , []);
 
     return (
         <div className="result-list">
-            {/* <h2> List Contacs </h2> */}
-            <h6> {results.length} </h6>
+            <h6> {state.from} </h6>
             <div className="result-list__border-line">
                 <ul className="result-list__list">
                     <li className="result-list__item">Name</li>
@@ -41,13 +79,19 @@ const ResultList = ({ results }) => {
                     <li className="result-list__item">Country</li>
                 </ul>
             </div>
-            <FlatList
+            {/* <FlatList
                 list={results}
                 renderItem={renderPerson}
                 renderWhenEmpty={() => <div>List Empty!</div>}
-                limit={5}
-                onMouseOver={mouseOverHandler}
-            />            
+                limit={limit}
+            /> */}
+            {results.map(renderPerson)}
+            <button onClick={()=> dispatch({type: 'increment'})}>Вперед</button>
+            <button onClick={()=> dispatch({type: 'decrement'})}>Назад</button>
+            {/* <form onSubmit={keyExtractor}>
+                <input type="submit" value="Назад"/>
+                <input type="submit" value="Вперед"/>
+            </form> */}
         </div>
     );
 };
